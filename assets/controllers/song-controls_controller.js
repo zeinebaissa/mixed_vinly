eeecb3a0dfb33af6a4256649e1bc08d461dc88ad
zeinebaissa <127880125+zeinebaissa@users.noreply.github.com>
2024-03-11 11:01:01 +0100

    first commit

---
+++ assets/controllers/song-controls_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from '@hotwired/stimulus';
+import axios from 'axios';
+
+export default class extends Controller {
+    static values = {
+        infoUrl: String,
+    };
+
+    play(event) {
+        event.preventDefault();
+
+        const currentAudio = this.element.dataset.currentAudio;
+
+
+
+        if (currentAudio && !currentAudio.paused) {
+            currentAudio.pause();
+
+        }
+
+        axios.get(this.infoUrlValue)
+            .then((response) => {
+                const audio = new Audio(response.data.url);
+
+                audio.play();
+            });
+    }
+}
--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -1,36 +1,40 @@
 <?php
+
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class VinylController extends AbstractController
+class VinylController  extends AbstractController
 {
-    #[Route('/')]
-    public function homepage() : Response{
-        $tracks= [
-            ['title' => 'Track 1', 'length' => '3:24'],
-            ['title' => 'Track 2', 'length' => '4:02'],
-            ['title' => 'Track 3', 'length' => '2:45'],
-            ['title' => 'Track 4', 'length' => '3:45'],
-            ['title' => 'Track 5', 'length' => '4:15'],
+    #[Route("/", name: 'app_homepage')]
+    public function homepage(): Response
+    {
+        $tracks = [
+            ['song' => 'Set Fire To The Rain', 'artist' => 'Adele'],
+            ['song' => 'This Is The Life', 'artist' => 'Amy Macdonald'],
+            ['song' => 'The Lazy Song', 'artist' => 'Bruno Mars'],
+            ['song' => 'Mockinggbird', 'artist' => 'Eminem'],
+            ['song' => 'Price Tag', 'artist' => 'Jessie J'],
+            ['song' => 'I Got You', 'artist' => 'James Brown'],
+            ['song' => 'Dark Horse', 'artist' => 'Katy Perry'],
 
         ];
-
         return $this->render('vinyl/homepage.html.twig', [
-            'tracks' => $tracks,'title' => 'Vinyl Shop',
+            'title' => 'PB & Jams',
+            'tracks' => $tracks,
         ]);
     }
+    #[Route('/browse/{slug}', name: 'app_browse')]
 
-    #[Route('/browse/{slug}')]
-    public function browse(string $slug = null) : Response{
-        if($slug){
-            $title = 'Genre : '.u(str_replace('-', ' ', $slug))->title(true);
-        }else{
-            $title = 'All Genres';
-        }
-        return new Response($title);
+    public function browse($slug = null): Response
+    {
+        $genre  = $slug ? 'Genre: ' . u(str_replace('-', ' ', $slug))->title(true) : null;
+
+        return $this->render('vinyl/browse.html.twig', [
+            'genre' => $genre,
+        ]);
     }
 }
